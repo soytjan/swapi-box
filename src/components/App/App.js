2bdb9c43f8604486fbb53f54b6cbd5797fbf1288
Amanda Tjan <amanda.tjan@gmail.com>
2018-01-31 20:31:43 -0700

    Implement API function from helper file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getApiData } from '../../helper';
 import Nav from '../Nav/Nav';
 import Main from '../Main/Main';
 import Header from '../Header/Header';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      people: [],
+      vehicles: [],
+      planets: [],
+      favorites: [],
+    }
+  }
+
+  handleNavClick = async (request) => {
+    console.log('clicked');
+    console.log(request);
+    console.log(this);
+    const data = await getApiData(request);
+
+    this.setState({[request]: data});
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <Nav />
+        <Nav onClick={this.handleNavClick}/>
         <Main />
       </div>
     );
