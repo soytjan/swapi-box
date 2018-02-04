b0a212104df8aa96588b8527fde2ff97e67786fb
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-04 16:06:14 -0700

    Add in stubs for App tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -21,5 +21,30 @@ describe('App', () => {
 
     expect(renderedComponent).toMatchSnapshot();
   });
+
+  it('initial state starts with a films array and a randomized film object', () => {
+
+  });
+
+  it('when user first clicks on People nav button and the page is populated with people data and people state array is set', () => {
+
+  });
+
+  it('when user first clicks on Planets nav button and the page is populated with planets data and planets state array is set', () => {
+
+  });
+
+  it('when user first clicks on Vehicles nav button and the page is populated with vehicles data and vehicles state array is set', () => {
+
+  });
+
+  it('when user clicks Favorite button on card, it is added to the favorites state array', () => {
+
+  });
+
+  it('when user clicks Favorite button on card and it is already in the array, it is removed from the favorites state array', () => {
+
+  });
+
 })
 
