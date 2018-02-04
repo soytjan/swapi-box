9c35dc273436a78b5f7869a4edcde3af90d3639a
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-03 18:52:19 -0700

    Add in snapshot tests for App, Card, and Header

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 import { shallow } from 'enzyme';
-
+import App from './App';
+import { getApiData } from '../../helper';
+import { mockFilmData } from '../../mockData';
 
 describe('App', () => {
   it('expect to exist', () => {
     expect(App).toBeDefined();
   })
 
-  it.skip('should match the snapshot', () => {
-    const renderedComponent = shallow(<App />);
+  it.skip('should match the snapshot', async () => {
+    window.fetch = jest.fn().mockImplementation(() => ({
+        status: 200,
+        json: () => new Promise((resolve, reject) => {
+          resolve(mockFilmData) 
+        })
+      }))
+
+    const renderedComponent = await shallow(<App />);
 
     expect(renderedComponent).toMatchSnapshot();
   });
--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Card from './Card';
 
 describe('Card', () => {
   it('expect to exist', () => {
     expect(Card).toBeDefined();
+  })
+
+  it.skip('should match snapshot', () => {
+    const mockInfo = {name: "Coruscant", terrain: "cityscape, mountains", population: "1000000000000", climate: "temperate", residents: ["Finis Valorum", "Adi Gallia", "Jocasta Nu"]};
+    const mockFavClick = jest.fn();
+    const renderedComponent = shallow(<Card info={mockInfo} type='planets' isFavorited={false} onFavClick={mockFavClick} key={1} />)
+
+    expect(renderedComponent).toMatchSnapshot();
   })
 })
