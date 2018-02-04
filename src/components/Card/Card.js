79bd1b3ff12eee92332ed08f6ed3f540319cc5e6
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-04 15:04:27 -0700

    Lint Card first pass

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,27 +4,35 @@ import './Card.css';
 
 const Card = ({info, type, onFavClick, isFavorited}) => {
   const attributes = Object.keys(info).filter((attr) => attr !== 'name');
-  const klass = `card ${type} ${isFavorited}`
+  const klass = `card ${type} ${isFavorited}`;
   const listItems = attributes.map((attr, index) => {
     let value = info[attr];
     if (typeof value === 'object') {
       value = value.join(', ');
     }
 
-    return <li key={index}><div className='attr'>{attr}:</div> <div className='value'> {value}</div></li>
+    return (
+      <li key={index}>
+        <div className='attr'>{attr}:</div> 
+        <div className='value'> {value}</div>
+      </li>
+    );
   });
 
   return ( 
     <article className={klass}>
       <div className='card-header'>
         <h4>{info.name}</h4>
-        <button onClick={() => onFavClick(info, isFavorited)} className='star-btn'></button>
+        <button 
+          onClick={() => onFavClick(info, isFavorited)} 
+          className='star-btn'>
+        </button>
       </div>
       <ul>
         {listItems}
       </ul>
     </article>
-  )
+  );
 }
 
 Card.propTypes = {
