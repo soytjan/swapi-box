29ac0d8ef2c3696242468607ebcbaee458f0f05f
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-04 19:38:05 -0700

    Write Card tests

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -3,36 +3,48 @@ import { shallow } from 'enzyme';
 import Card from './Card';
 
 describe('Card', () => {
-  it('expect to exist', () => {
-    expect(Card).toBeDefined();
+  let mockInfo;
+  let mockFavClick;
+
+  beforeAll(() => {
+    mockInfo = {
+      name: "Coruscant", 
+      terrain: "cityscape, mountains", 
+      population: "1000000000000", 
+      climate: "temperate", 
+      residents: ["Finis Valorum", "Adi Gallia", "Jocasta Nu"]};
+
+    mockFavClick = jest.fn();
   })
 
-  it.skip('should match snapshot', () => {
-    const mockInfo = {name: "Coruscant", terrain: "cityscape, mountains", population: "1000000000000", climate: "temperate", residents: ["Finis Valorum", "Adi Gallia", "Jocasta Nu"]};
-    const mockFavClick = jest.fn();
-    const renderedComponent = shallow(<Card info={mockInfo} type='planets' isFavorited={false} onFavClick={mockFavClick} key={1} />)
+  it('should have a class of people when it has people data', () => {
+    const renderedComponent = shallow(<Card info={mockInfo} type='people' isFavorited={''} onFavClick={mockFavClick} key={1} />)
 
     expect(renderedComponent).toMatchSnapshot();
   });
 
-  it('should have a class of people when it has people data', () => {
-
-  });
-
   it('should have a class of planets when it has planets data', () => {
+    const renderedComponent = shallow(<Card info={mockInfo} type='planets' isFavorited={''} onFavClick={mockFavClick} key={1} />)
 
+    expect(renderedComponent).toMatchSnapshot();
   });
 
   it('should have a class of vehicles when it has vehicles data', () => {
+    const renderedComponent = shallow(<Card info={mockInfo} type='vehicles' isFavorited={''} onFavClick={mockFavClick} key={1} />)
 
+    expect(renderedComponent).toMatchSnapshot();
   });
 
   it('should have a class of favorites when it is in the favorites section', () => {
-
+    const renderedComponent = shallow(<Card info={mockInfo} type='planets' isFavorited={'favorited'} onFavClick={mockFavClick} key={1} />)
+    
+    expect(renderedComponent).toMatchSnapshot();
   });
 
   it('should call onFavClick function when button is clicked', () => {
-    const mockFavClick = jest.fn();
-    
+    const renderedComponent = shallow(<Card info={mockInfo} type='planets' isFavorited={''} onFavClick={mockFavClick} key={1} />)
+
+    renderedComponent.find('button').simulate('click');
+    expect(mockFavClick.mock.calls.length).toBe(1)
   });
 })
